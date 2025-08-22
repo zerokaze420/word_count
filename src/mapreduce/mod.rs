d7207137e3350d2fb6aa9a9412c420b7d2534a2a
hashcowuwu <3189506476@qq.com>
2025-08-23 00:13:45 +0800

    feat(mapreduce): implement shuffle_and_group function
    - Add shufffle_and_group function to reducer.rs
    - Update mod.rs to expose the new function
    - This function groups the map output by key, preparing for the reduce step

--- src/mapreduce/mod.rs
+++ src/mapreduce/mod.rs
@@ -1,4 +1,4 @@
 pub mod mapper;
 pub mod reducer;
 pub use mapper::map;
-pub use reducer::reduce;
+pub use reducer:: {reduce, shufffle_and_group};
--- src/mapreduce/reducer.rs
+++ src/mapreduce/reducer.rs
@@ -1,8 +1,19 @@
+use std::collections::HashMap;
 
 pub fn reduce(_word: &str, counts: Vec<i32>) -> i32 {
     counts.iter().sum()
 }
 
+
+pub fn shufffle_and_group(map_output:   Vec<(String, i32)>) -> HashMap<String, Vec<i32>>{
+    let mut grouped_data = HashMap::new();
+
+    for (word, count) in map_output {
+        grouped_data.entry(word).or_insert(Vec::new()).push(count);
+    }
+    grouped_data
+
+}
 
 #[cfg(test)]
 mod tests {
